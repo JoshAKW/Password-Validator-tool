ee0d9b7a514168938acba5b56208f8d198f5fab8
Joshua West <JoshuaHWest@gmail.com>
2022-04-16 16:54:20 -0600

    Finished Variables and SLDC

---
+++ sldc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle
+     and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined.\
+ Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This is the second step, where the product requirments are specified, documented and approved."
+
+const design = "This is the third step, where the architecture of the product is developed, usually there are several aproaches proposed\
+ based on the product requirements, as well as other concerns (such as time, budget, robustness, modularity etc.)\
+ the best design will be selected."
+
+const implementation = "This is the fourth step, where the product is developed and built. The coding is generated per\
+ the previous stages results."
+
+const testingIntegration = "This is the fifth step, where the product is tested for defects. Defects are reported, fixed, and retested.\
+ Beta testers are often used in this regard (I think haha)"
+
+const maintain = "This is the final and continous step, where the product is maintained in the face of updates,\
+ changing system requirements etc."
